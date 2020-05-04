4837343bea4fa9c2958213870895826c5ae5819c
nabhiraj <nabhiraj96@gmail.com>
2020-05-05 00:41:10 +0530

    sirial code basic functioin working

--- mirror.cpp
+++ mirror.cpp
@@ -1,27 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
+#include <complex.h>
 #include<fftw3.h>
 #define ind_ele(n,i,j) n*(i)+j
 #define get_start(l_s,k_s,x) (x-k_s>=0)?x-k_s:(l_s+(x-k_s))
 #define inc(l_s,x) (x+1>=l_s)?0:x+1
 
 using namespace std;
-double* create_matrix(int n){
-    double* mat=new double[n*n];
+void flip_col(float* mat,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+            float temp;
+            temp=mat[ind_ele(n,i,j)];
+            mat[ind_ele(n,i,j)]=mat[ind_ele(n,i,n-1-j)];
+            mat[ind_ele(n,i,n-1-j)]=temp;
+        }
+    }
+}
+void flip_row(float* mat,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+            float temp;
+            temp=mat[ind_ele(n,j,i)];
+            mat[ind_ele(n,j,i)]=mat[ind_ele(n,n-j-1,i)];
+            mat[ind_ele(n,n-j-1,i)]=temp;
+        }
+    }
+}
+float* create_matrix(int n){
+    float* mat=new float[n*n];
     return mat;
 }
-void fill_random(double* mat,int n){
+void fill_random(float* mat,int n){
     for(int i=0;i<n*n;i++){
         mat[i]=rand()%10;
     }
 }
-void fill_constant(double* mat,double c,int n){
+void fill_constant(float* mat,float c,int n){
     for(int i=0;i<n*n;i++){
         mat[i]=c;
     }
 }
-void fill_user(double* mat,int n){
+void fill_user(float* mat,int n){
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cout<<"--> ["<<i<<"]"<<"["<<j<<"]"<<endl;
@@ -29,7 +50,7 @@ void fill_user(double* mat,int n){
         }
     }
 }
-void print_mat(double* arr,int n){
+void print_mat(float* arr,int n){
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cout<<arr[ind_ele(n,i,j)]<<" ";
@@ -37,13 +58,24 @@ void print_mat(double* arr,int n){
         cout<<endl;
     }
 }
-void conv(double* layer,double* kernel,int layer_size,int kernel_size,double* output){
+void print_comp_mat(fftwf_complex* arr,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"("<<arr[ind_ele(n,i,j)][0]<<","<<arr[ind_ele(n,i,j)][1]<<") ";
+        }
+        cout<<endl;
+    
+}
+}
+void conv(float* layer,float* kernel,int layer_size,int kernel_size,float* output){
+    flip_row(kernel,kernel_size);
+    flip_col(kernel,kernel_size);
     for(int i=0;i<layer_size;i++){
         for(int j=0;j<layer_size;j++){
             int k_s=(kernel_size-1)/2;
             int l_row_start=get_start(layer_size,k_s,i);
             int l_col_start=get_start(layer_size,k_s,j);
-            double sum=0;
+            float sum=0;
             for(int ik=0;ik<kernel_size;ik++){
                 l_col_start=get_start(layer_size,k_s,j);
                 for(int jk=0;jk<kernel_size;jk++){
@@ -56,30 +88,10 @@ void conv(double* layer,double* kernel,int layer_size,int kernel_size,double* ou
         }
     }
 }
-void flip_col(double* mat,int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n/2;j++){
-            double temp;
-            temp=mat[ind_ele(n,i,j)];
-            mat[ind_ele(n,i,j)]=mat[ind_ele(n,i,n-1-j)];
-            mat[ind_ele(n,i,n-1-j)]=temp;
-        }
-    }
-}
-void flip_row(double* mat,int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n/2;j++){
-            double temp;
-            temp=mat[ind_ele(n,j,i)];
-            mat[ind_ele(n,j,i)]=mat[ind_ele(n,n-j-1,i)];
-            mat[ind_ele(n,n-j-1,i)]=temp;
-        }
-    }
-}
-double* preProcessKernel(double* plainKernel,int ini_size,int final_size){//i only will do it.
+float* preProcessKernel(float* plainKernel,int ini_size,int final_size){//i only will do it.
     flip_col(plainKernel,ini_size);
     flip_row(plainKernel,ini_size);
-    double* res_mat=create_matrix(final_size);
+    float* res_mat=create_matrix(final_size);
     fill_constant(res_mat,0,final_size);
     int ini_row=final_size-1;
     int ini_col=final_size-1;
@@ -102,28 +114,56 @@ void mul(fftwf_complex* A,fftwf_complex* B,fftwf_complex* output,int n){
         }
     }
 }
-
+void scale_comp(fftwf_complex* mat,int n){
+    int nn=n*n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            mat[ind_ele(n,i,j)][0]=mat[ind_ele(n,i,j)][0]/nn;
+        }
+    }
+}
+//routine to convert a float matrix to a complex matrix.
+fftwf_complex* get_complex_representation(float* mat,int n){
+    fftwf_complex* res=new fftwf_complex[n*n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            res[ind_ele(n,i,j)][0]=mat[ind_ele(n,i,j)];
+            res[ind_ele(n,i,j)][1]=0;
+        }
+    }
+    return res;
+}
 //this method is not tested yet.
-void fft_conv(float* layer,float* kernel,int filter_size,float* output){
-    fftwf_complex* fft_layer=new fftwf_complex[filter_size*filter_size];
-    fftwf_complex* fft_kernel=new fftwf_complex[filter_size*filter_size];
-    fftwf_plan get_layer=fftwf_plan_dft_r2c_2d(filter_size,filter_size,layer,fft_layer,FFTW_ESTIMATE);
-    fftwf_plan get_kernel=fftwf_plan_dft_r2c_2d(filter_size,filter_size,kernel,fft_kernel,FFTW_ESTIMATE);
+//there is mistake in inverse forirer transform.
+void fft_conv(float* layer,float* kernel,int filter_size,fftwf_complex* output){
+    fftwf_complex* fft_layer;
+    fft_layer=(fftwf_complex*)fftw_malloc(sizeof(fftwf_complex)*filter_size*filter_size);
+    fftwf_complex* fft_kernel;
+    fft_kernel=(fftwf_complex*)fftw_malloc(sizeof(fftwf_complex)*filter_size*filter_size);
+    fftwf_plan get_layer=fftwf_plan_dft_2d(filter_size,filter_size,get_complex_representation(layer,filter_size),fft_layer,FFTW_FORWARD,FFTW_ESTIMATE);
+    fftwf_plan get_kernel=fftwf_plan_dft_2d(filter_size,filter_size,get_complex_representation(kernel,filter_size),fft_kernel,FFTW_FORWARD,FFTW_ESTIMATE);
     //now we need to multiply.
-    fftwf_complex* fft_mul=new fftwf_complex[filter_size*filter_size];
+    fftwf_complex* fft_mul=(fftwf_complex*)fftw_malloc(sizeof(fftwf_complex)*filter_size*filter_size);
     fftwf_execute(get_layer);
     fftwf_execute(get_kernel);
+    cout<<"fft of the layer is "<<endl;
+    print_comp_mat(fft_layer,filter_size);
+    cout<<"fft of the kernel is "<<endl;
+    print_comp_mat(fft_kernel,filter_size);
     mul(fft_layer,fft_kernel,fft_mul,filter_size);
-    fftwf_plan get_realMul=fftwf_plan_dft_c2r_2d(filter_size,filter_size,fft_mul,output,FFTW_ESTIMATE);
+    cout<<"their multiplication is "<<endl;
+    print_comp_mat(fft_mul,filter_size);
+    fftwf_plan get_realMul=fftwf_plan_dft_2d(filter_size,filter_size,fft_mul,output,FFTW_BACKWARD,FFTW_ESTIMATE);
     fftwf_execute(get_realMul);
+    scale_comp(output,filter_size);
 }
 void messaure_normal_time(){
     int layer_length=512;
-    double* layer=create_matrix(layer_length);
+    float* layer=create_matrix(layer_length);
     fill_random(layer,layer_length);
-    double* output=create_matrix(layer_length);
+    float* output=create_matrix(layer_length);
     for(int kernel_length=3;kernel_length<512;kernel_length+=2){
-        double* kernel=create_matrix(kernel_length);
+        float* kernel=create_matrix(kernel_length);
         fill_random(kernel,kernel_length);
         clock_t start_time;
         clock_t end_time;
@@ -134,20 +174,31 @@ void messaure_normal_time(){
         end_time=clock();
         double clock_taken=double(end_time - start_time);
         clock_taken=clock_taken/10;
-        double time_taken=clock_taken/double(CLOCKS_PER_SEC);
+        double time_taken=clock_taken/float(CLOCKS_PER_SEC);
         cout<<kernel_length<<" "<<time_taken<<endl;
         delete kernel;
     }
 }
 int main(){
     //testing the flipping operation.
-    double* test_mat=create_matrix(3);
-    fill_user(test_mat,3);
-    flip_col(test_mat,3);
-    cout<<"after the fliping the col"<<endl;
-    print_mat(test_mat,3);
-    cout<<"after fliping the row"<<endl;
-    flip_row(test_mat,3);
-    print_mat(test_mat,3);
+    float* l=create_matrix(5);
+    float* k=create_matrix(3);
+    cout<<"enter the elements of the layler"<<endl;
+    fill_user(l,5);
+    cout<<"enter the elemnts of the kernel"<<endl;
+    fill_user(k,3);
+    float* ck=preProcessKernel(k,3,5);
+    //float* output=create_matrix(5);
+    fftwf_complex* output=new fftwf_complex[5*5];
+    float* output2=create_matrix(5);
+    fft_conv(l,ck,5,output);
+    conv(l,k,5,3,output2);
+    cout<<"the convolution operation gives"<<endl;
+    print_comp_mat(output,5);
+    print_mat(output2,5);
     return 0;
 }
+
+
+//layer=[[1,1,1,2,1],[8,3,2,6,2],[4,5,6,3,3],[1,1,1,1,1],[3,3,3,4,5]]
+//kernel=[[1,1,2],[3,1,2],[4,5,5]]
